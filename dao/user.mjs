2e73eda75ebec8e33a6c2df9d04e5b89336a0472
kjm90110 <kjm90118730@gmail.com>
2025-12-16 17:55:08 +0900

    유저 프로필 작업 중

--- dao/user.mjs
+++ dao/user.mjs
@@ -1,35 +1,59 @@
 import User from "../models/User.js";
 
 export async function findByUserid(userid) {
-  return User.findOne({ userid });
+    return User.findOne({ userid });
 }
 
 export async function findByEmail(email) {
-  return User.findOne({ email });
+    return User.findOne({ email });
 }
 
 export async function findByUseridWithPassword(userid) {
-  return User.findOne({ userid }).select("+password");
+    return User.findOne({ userid }).select("+password");
 }
 
 export async function createUser({
-  userid,
-  password,
-  nickname,
-  email,
-  profileImg = null,
-}) {
-  const user = new User({
     userid,
     password,
     nickname,
     email,
-    profileImg,
-  });
+    profileImg = null,
+}) {
+    const user = new User({
+        userid,
+        password,
+        nickname,
+        email,
+        profileImg,
+    });
 
-  return user.save();
+    return user.save();
 }
 
 export async function findById(userId) {
-  return User.findById(userId).select("-password");
+    return User.findById(userId).select("-password");
+}
+
+export async function updatePassword(newPw) {}
+
+export async function updateProfile(userId, nickname, email, bio, profileImg) {
+    // 만약에 이메일을 새로 수정했는데
+    // 다른 사람이 그 이메일을 쓰고 있을 경우 수정 안 되도록
+    const emailOwner = await User.findOne({ email });
+    if (emailOwner) return false;
+
+    const updatedUser = await User.findOneAndUpdate(
+        { _id: userId },
+        {
+            $set: {
+                nickname: nickname,
+                email: email,
+                bio: bio,
+                profileImg: profileImg,
+            },
+        },
+        { new: true }
+    );
+
+    return updatedUser;
 }
--- router/user.mjs
+++ router/user.mjs
@@ -7,52 +7,52 @@ import { isAuth } from "../middleware/auth.mjs";
 const router = express.Router();
 
 const validateLogin = [
-  body("userid")
-    .trim()
-    .notEmpty()
-    .withMessage("아이디를 입력해주세요")
-    .isLength({ min: 4, max: 20 })
-    .withMessage("아이디는 4~20자여야 합니다")
-    .matches(/^[a-zA-Z0-9]+$/)
-    .withMessage("아이디는 영문과 숫자만 사용 가능합니다"),
+    body("userid")
+        .trim()
+        .notEmpty()
+        .withMessage("아이디를 입력해주세요")
+        .isLength({ min: 4, max: 20 })
+        .withMessage("아이디는 4~20자여야 합니다")
+        .matches(/^[a-zA-Z0-9]+$/)
+        .withMessage("아이디는 영문과 숫자만 사용 가능합니다"),
 
-  body("password").trim().notEmpty().withMessage("비밀번호를 입력해주세요"),
+    body("password").trim().notEmpty().withMessage("비밀번호를 입력해주세요"),
 
-  validate,
+    validate,
 ];
 
 const validateSignup = [
-  body("userid")
-    .trim()
-    .notEmpty()
-    .withMessage("아이디를 입력해주세요")
-    .isLength({ min: 4, max: 20 })
-    .withMessage("아이디는 4~20자여야 합니다")
-    .matches(/^[a-zA-Z0-9]+$/)
-    .withMessage("아이디는 영문과 숫자만 사용 가능합니다"),
+    body("userid")
+        .trim()
+        .notEmpty()
+        .withMessage("아이디를 입력해주세요")
+        .isLength({ min: 4, max: 20 })
+        .withMessage("아이디는 4~20자여야 합니다")
+        .matches(/^[a-zA-Z0-9]+$/)
+        .withMessage("아이디는 영문과 숫자만 사용 가능합니다"),
 
-  body("password")
-    .trim()
-    .notEmpty()
-    .withMessage("비밀번호를 입력해주세요")
-    .isLength({ min: 8 })
-    .withMessage("비밀번호는 8자 이상이어야 합니다"),
+    body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("비밀번호를 입력해주세요")
+        .isLength({ min: 8 })
+        .withMessage("비밀번호는 8자 이상이어야 합니다"),
 
-  body("nickname")
-    .trim()
-    .notEmpty()
-    .withMessage("닉네임을 입력해주세요")
-    .isLength({ min: 2, max: 10 })
-    .withMessage("닉네임은 2~10자여야 합니다"),
+    body("nickname")
+        .trim()
+        .notEmpty()
+        .withMessage("닉네임을 입력해주세요")
+        .isLength({ min: 2, max: 10 })
+        .withMessage("닉네임은 2~10자여야 합니다"),
 
-  body("email")
-    .trim()
-    .notEmpty()
-    .withMessage("이메일을 입력해주세요")
-    .isEmail()
-    .withMessage("올바른 이메일 형식이 아닙니다"),
+    body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("이메일을 입력해주세요")
+        .isEmail()
+        .withMessage("올바른 이메일 형식이 아닙니다"),
 
-  validate,
+    validate,
 ];
 
 // 회원가입
@@ -63,5 +63,11 @@ router.post("/login", validateLogin, userController.login);
 
 // 로그인 유지 확인
 router.post("/me", isAuth, userController.me);
+
+// 유저가 프로필 내용 변경 시 여기로
+router.put("/updtate_profile", isAuth, userController.updateProfile);
+
+// 비밀번호 변경
+// router.put('/update_pw', isAuth, userController.)
 
 export default router;
