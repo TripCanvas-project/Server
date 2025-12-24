dcf67371fc39ec31f800ab1e2655fa9b9a2e1545
LeF-0213 <solsol815815@gmail.com>
2025-12-24 16:28:54 +0900

    merge 해결 trip

--- router/trip.mjs
+++ router/trip.mjs
@@ -2,7 +2,6 @@ import express from "express";
 import { isAuth } from "../middleware/auth.mjs";
 import Trip from "../models/Trip.mjs";
 import * as tripController from "../controller/trip.mjs";
-const router = express.Router();
 
 const router = express.Router();
 
@@ -40,5 +39,4 @@ router.get("/:tripId", isAuth, async (req, res) => {
         return res.status(500).json({ message: "여행 정보 조회 중 오류가 발생했습니다." });
     }
 });
-
 export default router;
