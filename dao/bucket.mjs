5a5e04e76e8a1e7e2a9213e5ad8a71bfe6a9fc67
고민승 <minseung5621@naver.com>
2025-12-30 15:36:45 +0900

    버켓리스트 기능추가

--- dao/bucket.mjs
+++ dao/bucket.mjs
@@ -1,5 +1,5 @@
 import Bucketlist from "../models/Bucket.js";
 
 export const getBucketsByUserId = async (userId) => {
-    return await Bucketlist.find({ userId }).sort({ createdAt: -1 }).exec();
+  return await Bucketlist.find({ userId }).sort({ createdAt: -1 }).exec();
 };
